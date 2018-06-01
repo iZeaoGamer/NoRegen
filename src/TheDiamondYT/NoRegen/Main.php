1933e523ba155b29606bf2bfa137a27a673422a7
iZeaoGamer <jameshayward14@icloud.com>
2018-06-01 19:07:25 +0100

    Fixed Bugs.

--- src/TheDiamondYT/NoRegen/Main.php
+++ src/TheDiamondYT/NoRegen/Main.php
@@ -14,30 +14,30 @@ class Main extends PluginBase implements Listener {
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
     }
     public function onPlayerJoin(PlayerJoinEvent $ev) {
-        $config = $this->getConfig()->getAll();
+        $config = $this->getConfig()->get();
         
-        if($config["message"]["show"]) {
-            $ev->getPlayer()->sendMessage($config["message"]["text"]);
+        if($this->getConfig()->get("message")) {
+            $ev->getPlayer()->sendMessage($this->getConfig()->get("message"));
         }
     }
     
     public function onRegainHealth(EntityRegainHealthEvent $ev) {
-        $block = $this->getConfig()->getAll()["block"];
+        $block = $this->getConfig()->get("block");
         $reason = $ev->getRegainReason();
         
-        if($block["regen"] && $reason === EntityRegainHealthEvent::CAUSE_REGEN) {
+        if($this->getConfig()->get("regen") && $reason === EntityRegainHealthEvent::CAUSE_REGEN) {
             $ev->setCancelled(true);
         }
-        if($block["eating"] && $reason === EntityRegainHealthEvent::CAUSE_EATING) {
+        if($this->getConfig()->get("eating") && $reason === EntityRegainHealthEvent::CAUSE_EATING) {
             $ev->setCancelled(true);
         }
-        if($block["potion"] && $reason === EntityRegainHealthEvent::CAUSE_MAGIC) {
+        if($this->getConfig()->get("potion") && $reason === EntityRegainHealthEvent::CAUSE_MAGIC) {
             $ev->setCancelled(true);
         }
-        if($block["custom"] && $reason === EntityRegainHealthEvent::CAUSE_CUSTOM) {
+        if($this->getConfig()->get("custom") && $reason === EntityRegainHealthEvent::CAUSE_CUSTOM) {
             $ev->setCancelled(true);
         }
-        if($block["saturation"] && $reason === EntityRegainHealthEvent::CAUSE_SATURATION) {
+        if($this->getConfig()->get("saturation") && $reason === EntityRegainHealthEvent::CAUSE_SATURATION) {
             $ev->setCancelled(true);
         }
     }
